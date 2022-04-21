7738cb6ab5f38ec472f73f0562e289c2b4c460bd
Lucien Perouze <lucien.perouze@cezemb.re>
2022-04-21 13:24:24 +0200

    Order list on cancel order

--- src/trading/orderList.tsx
+++ src/trading/orderList.tsx
@@ -4,9 +4,10 @@ import OrderThumbnail from './orderThumbnail';
 
 export interface Props {
   orders?: Order[];
+  onCancelOrder?: (order: Order) => unknown;
 }
 
-export default function OrderList({ orders }: Props): ReactElement {
+export default function OrderList({ orders, onCancelOrder }: Props): ReactElement {
   if (!orders?.length) {
     return <span>Aucun order en cours</span>;
   }
@@ -15,7 +16,10 @@ export default function OrderList({ orders }: Props): ReactElement {
     <div className="friday-ui-order-list">
       {orders.map((order: Order) => (
         <div key={order.id} className="order">
-          <OrderThumbnail order={order} />
+          <OrderThumbnail
+            order={order}
+            onCancel={onCancelOrder ? () => onCancelOrder(order) : undefined}
+          />
         </div>
       ))}
     </div>
