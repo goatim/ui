dd2e0f5d06ddb3b4e9a340a55f45b3746a31eefd
Lucien Perouze <lucien.perouze@cezemb.re>
2022-07-25 11:52:16 +0200

    Responsive time !

--- src/soccer/clubs/clubList.tsx
+++ src/soccer/clubs/clubList.tsx
@@ -5,13 +5,16 @@ import ClubThumbnail from './clubThumbnail';
 
 export interface Props {
   clubs?: Club[];
+  columns?: number;
   clubOnClick?: (club: Club, event: MouseEvent<HTMLButtonElement>) => Promise<void> | void;
   clubTo?: (club: Club) => To;
 }
 
-export default function ClubList({ clubs, clubOnClick, clubTo }: Props): ReactElement {
+export default function ClubList({ clubs, columns = 3, clubOnClick, clubTo }: Props): ReactElement {
   return (
-    <div className="friday-ui-club-list">
+    <div
+      className="friday-ui-club-list"
+      style={{ gridTemplateColumns: `repeat(${columns}, auto)` }}>
       {clubs ? (
         clubs.map((club) => (
           <div key={club.id} className="club">
