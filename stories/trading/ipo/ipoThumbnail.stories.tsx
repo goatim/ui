b48c35a8311a7da24cb08a831d69d250160d3232
Philippe Ong <ong.philippe@gmail.com>
2022-08-14 13:49:19 +0200

    feat: create IPOThumbnail storybook

---
+++ stories/trading/ipo/ipoThumbnail.stories.tsx
@@ -0,0 +1,154 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { DateTime } from 'luxon';
+import { JSXElementConstructor } from 'react';
+import { Asset, Player, Club, QuotationHistory } from '@fridaygame/client';
+import IPOThumbnail, {
+  IPOThumbnailSize,
+  IPOThumbnailTheme,
+} from '../../../src/trading/ipo/ipoThumbnail';
+
+interface Props {
+  size?: IPOThumbnailSize;
+  theme?: IPOThumbnailTheme;
+  displayContent?: boolean;
+}
+
+export default {
+  title: 'Trading/IPOThumbnail',
+  component: IPOThumbnail,
+  argTypes: {
+    size: {
+      options: ['narrow', 'small', 'medium', 'big', 'full', 'stretch'],
+      control: {
+        type: 'select',
+      },
+    },
+    theme: {
+      options: ['default', 'dark', 'light'],
+      control: {
+        type: 'select',
+      },
+    },
+    displayContent: {
+      options: [true, false],
+      control: {
+        type: 'select',
+      },
+    },
+  },
+} as ComponentMeta<JSXElementConstructor<Props>>;
+
+const club: Club = {
+  id: '1',
+  name: 'Paris Saint-Germain',
+  slug: 'paris-saint-germain',
+  description: '',
+  icon: {
+    id: '1',
+    url: 'https://www.psg.fr/img/logos/psg-logo.png',
+    large_url: 'https://upload.wikimedia.org/wikipedia/fr/7/76/Paris_Saint-Germain_logo.png',
+    medium_url: 'https://upload.wikimedia.org/wikipedia/fr/7/76/Paris_Saint-Germain_logo.png',
+    small_url: 'https://www.psg.fr/img/logos/psg-logo.png',
+    thumbnail_url: 'https://www.psg.fr/img/logos/psg-logo.png',
+  },
+};
+
+const player: Player = {
+  id: '1',
+  club,
+  name: 'Kylian Mbappé',
+  number: 10,
+  position: 'attacking_midfield',
+  side: 'left',
+  textual_position: 'Attaquant centre',
+};
+
+const quotationHistory: QuotationHistory = {
+  data: [
+    {
+      t: 1646755289000,
+      a: 42,
+      o: 0,
+      c: 0,
+      h: 0,
+      l: 0,
+      v: 0,
+    },
+    {
+      t: 1647273689000,
+      a: 75,
+      o: 0,
+      c: 0,
+      h: 0,
+      l: 0,
+      v: 0,
+    },
+    {
+      t: 1647373889000,
+      a: 23,
+      o: 0,
+      c: 0,
+      h: 0,
+      l: 0,
+      v: 0,
+    },
+    {
+      t: 1647532889000,
+      a: 125,
+      o: 0,
+      c: 0,
+      h: 0,
+      l: 0,
+      v: 0,
+    },
+    {
+      t: 1648137689000,
+      a: 23,
+      o: 0,
+      c: 0,
+      h: 0,
+      l: 0,
+      v: 0,
+    },
+  ],
+  variation: 0.25,
+};
+
+const asset: Asset = {
+  id: 'as_Ded512',
+  entity: 'pl_de45d54DD',
+  type: 'player',
+  name: 'Kylian Mbappé',
+  description: '',
+  slug: 'kylian-mbappe',
+  total_shares: 450,
+  quotation: 2750,
+  session_variation: 345,
+  player,
+  quotation_history: quotationHistory,
+};
+
+const dateTime = DateTime.local(2030, 8, 14, 8, 30);
+
+const Template: ComponentStory<JSXElementConstructor<Props>> = ({
+  displayContent,
+  size,
+  theme,
+}: Props) => (
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+    }}>
+    <IPOThumbnail
+      asset={asset}
+      size={size}
+      theme={theme}
+      beginning={dateTime}
+      onClick={() => undefined}
+      displayContent={displayContent}
+    />
+  </div>
+);
+
+export const Default = Template.bind({});
