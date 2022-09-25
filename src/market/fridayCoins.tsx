f45b1663c0db14a6431a422a7067f64291b49aeb
Philippe Ong <ong.philippe@gmail.com>
2022-09-25 16:43:39 +0200

    add new prop decimalDigits and new theme gold to FridayCoins component

--- src/market/fridayCoins.tsx
+++ src/market/fridayCoins.tsx
@@ -3,20 +3,24 @@ import { formatFridayCoins } from '@fridaygame/client';
 
 export type FridayCoinsSize = 'small' | 'medium' | 'big' | 'large';
 
-export type FridayCoinsTheme = 'dark' | 'light' | 'darker';
+export type FridayCoinsTheme = 'dark' | 'light' | 'darker' | 'gold';
 
 export interface Props {
   amount?: number;
+  decimalDigits?: number;
   size?: FridayCoinsSize;
   theme?: FridayCoinsTheme;
 }
 
 export default function FridayCoins({
   amount = 0,
+  decimalDigits = 2,
   size = 'small',
   theme = 'dark',
 }: Props): ReactElement {
   return (
-    <span className={`friday-ui-friday-coins ${size} ${theme}`}>{formatFridayCoins(amount)}</span>
+    <span className={`friday-ui-friday-coins ${size} ${theme}`}>
+      {formatFridayCoins(amount, decimalDigits)}
+    </span>
   );
 }
