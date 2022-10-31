bb6fb0e6079641620afeadb63898b0cb55e64b84
Lucien Perouze <lucien.perouze@cezemb.re>
2022-10-31 15:42:54 +0100

    Forward refs

--- src/trading/assets/assetThumbnail.tsx
+++ src/trading/assets/assetThumbnail.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, ReactElement, useMemo } from 'react';
+import { ForwardedRef, forwardRef, ReactElement, useMemo } from 'react';
 import { Asset } from '@fridaygame/client';
 import { Wrapper, WrapperProps } from '@cezembre/fronts';
 import { To } from 'react-router-dom';
@@ -25,117 +25,125 @@ export interface Props extends WrapperProps {
   secondaryTo?: To;
 }
 
-export default function AssetThumbnail({
-  asset,
-  shape = 'box',
-  size = 'small',
-  theme = 'default',
-  showQuotation = true,
-  to,
-  onClick,
-  href,
-  target,
-  secondaryTo,
-}: Props): ReactElement {
-  const playerThumbnailSize = useMemo<PlayerThumbnailSize>(() => {
-    switch (size) {
-      case 'narrow':
-      case 'small':
-      case 'medium':
-        return 'small';
-      case 'big':
-      case 'large':
-        return 'medium';
-      case 'full':
-        return 'full';
-      default:
-        return 'small';
-    }
-  }, [size]);
+export default forwardRef<HTMLAnchorElement | HTMLButtonElement | HTMLDivElement, Props>(
+  function AssetThumbnail(
+    {
+      asset,
+      shape = 'box',
+      size = 'small',
+      theme = 'default',
+      showQuotation = true,
+      to,
+      onClick,
+      type,
+      href,
+      target,
+      secondaryTo,
+    }: Props,
+    ref: ForwardedRef<HTMLAnchorElement | HTMLButtonElement | HTMLDivElement>,
+  ): ReactElement {
+    const playerThumbnailSize = useMemo<PlayerThumbnailSize>(() => {
+      switch (size) {
+        case 'narrow':
+        case 'small':
+        case 'medium':
+          return 'small';
+        case 'big':
+        case 'large':
+          return 'medium';
+        case 'full':
+          return 'full';
+        default:
+          return 'small';
+      }
+    }, [size]);
 
-  const playerThumbnailTheme = useMemo<PlayerThumbnailTheme>(() => {
-    if (theme === 'default') {
-      return 'dark';
-    }
-    return 'light';
-  }, [theme]);
+    const playerThumbnailTheme = useMemo<PlayerThumbnailTheme>(() => {
+      if (theme === 'default') {
+        return 'dark';
+      }
+      return 'light';
+    }, [theme]);
 
-  const dayVariationSize = useMemo<PercentageVariationSize>(() => {
-    switch (size) {
-      case 'narrow':
-      case 'small':
-      case 'medium':
-        return 'small';
-      case 'big':
-      case 'full':
-        return 'medium';
-      default:
-        return 'small';
-    }
-  }, [size]);
+    const dayVariationSize = useMemo<PercentageVariationSize>(() => {
+      switch (size) {
+        case 'narrow':
+        case 'small':
+        case 'medium':
+          return 'small';
+        case 'big':
+        case 'full':
+          return 'medium';
+        default:
+          return 'small';
+      }
+    }, [size]);
 
-  const quotationSize = useMemo<FridayCoinsSize>(() => {
-    switch (size) {
-      case 'narrow':
-      case 'small':
-      case 'medium':
-        return 'small';
-      case 'big':
-      case 'full':
-        return 'medium';
-      default:
-        return 'small';
-    }
-  }, [size]);
+    const quotationSize = useMemo<FridayCoinsSize>(() => {
+      switch (size) {
+        case 'narrow':
+        case 'small':
+        case 'medium':
+          return 'small';
+        case 'big':
+        case 'full':
+          return 'medium';
+        default:
+          return 'small';
+      }
+    }, [size]);
 
-  const quotationTheme = useMemo<FridayCoinsTheme>(() => {
-    if (theme === 'default') {
-      return 'dark';
-    }
-    return 'light';
-  }, [theme]);
+    const quotationTheme = useMemo<FridayCoinsTheme>(() => {
+      if (theme === 'default') {
+        return 'dark';
+      }
+      return 'light';
+    }, [theme]);
 
-  return (
-    <Wrapper
-      className={`friday-ui-asset-thumbnail ${shape} ${size} ${theme}`}
-      to={to}
-      onClick={onClick}
-      href={href}
-      target={target}>
-      {showQuotation &&
-      size === 'large' &&
-      shape === 'box' &&
-      (asset.quotation_history?.data.length || 0) > 1 ? (
-        <div className="quotation">
-          <QuotationHistoryGraph quotationHistory={asset.quotation_history} />
-        </div>
-      ) : null}
-
-      <div className="container">
-        {asset.type === 'player' && asset.player && typeof asset.player === 'object' ? (
-          <PlayerThumbnail
-            player={asset.player}
-            size={playerThumbnailSize}
-            theme={playerThumbnailTheme}
-            clubTo={secondaryTo}
-          />
-        ) : null}
-
-        {showQuotation ? (
-          <div className="metrics">
-            <div className="day-variation">
-              <PercentageVariation
-                variation={asset.day_variation}
-                shape="filled"
-                size={dayVariationSize}
-              />
-            </div>
-            <div className="quotation">
-              <FridayCoins amount={asset.quotation} size={quotationSize} theme={quotationTheme} />
-            </div>
+    return (
+      <Wrapper
+        ref={ref}
+        className={`friday-ui-asset-thumbnail ${shape} ${size} ${theme}`}
+        to={to}
+        onClick={onClick}
+        type={type}
+        href={href}
+        target={target}>
+        {showQuotation &&
+        size === 'large' &&
+        shape === 'box' &&
+        (asset.quotation_history?.data.length || 0) > 1 ? (
+          <div className="quotation">
+            <QuotationHistoryGraph quotationHistory={asset.quotation_history} />
           </div>
         ) : null}
-      </div>
-    </Wrapper>
-  );
-}
+
+        <div className="container">
+          {asset.type === 'player' && asset.player && typeof asset.player === 'object' ? (
+            <PlayerThumbnail
+              player={asset.player}
+              size={playerThumbnailSize}
+              theme={playerThumbnailTheme}
+              clubTo={secondaryTo}
+            />
+          ) : null}
+
+          {showQuotation ? (
+            <div className="metrics">
+              <div className="day-variation">
+                <PercentageVariation
+                  variation={asset.day_variation}
+                  shape="filled"
+                  size={dayVariationSize}
+                />
+              </div>
+              <div className="quotation">
+                <FridayCoins amount={asset.quotation} size={quotationSize} theme={quotationTheme} />
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </Wrapper>
+    );
+  },
+);
