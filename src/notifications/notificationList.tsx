c6bfd0c58ceaf872f1bf11e92f39a48bddd6ef91
Lucien Perouze <lucien.perouze@cezemb.re>
2022-11-04 14:59:23 +0100

    Notifications event

--- src/notifications/notificationList.tsx
+++ src/notifications/notificationList.tsx
@@ -1,19 +1,40 @@
-import { ReactElement } from 'react';
+import { MouseEvent, ReactElement } from 'react';
 import { Notification } from '@fridaygame/client';
+import { To } from 'react-router-dom';
 import NotificationThumbnail from './notificationThumbnail';
 
 export interface Props {
   notifications?: Notification[];
   colored?: boolean;
+  toNotification?: (notification: Notification) => To;
+  onNotificationClick?: (
+    notification: Notification,
+  ) => (event: MouseEvent<HTMLButtonElement>) => unknown;
+  notificationHref?: (notification: Notification) => string;
+  notificationTarget?: (notification: Notification) => string;
 }
 
-export default function NotificationList({ notifications, colored = false }: Props): ReactElement {
+export default function NotificationList({
+  notifications,
+  colored = false,
+  toNotification,
+  onNotificationClick,
+  notificationHref,
+  notificationTarget,
+}: Props): ReactElement {
   return (
     <div className="friday-ui-notification-list">
       {notifications?.length ? (
         notifications.map((notification) => (
           <div className="notification" key={notification.id}>
-            <NotificationThumbnail notification={notification} colored={colored} />
+            <NotificationThumbnail
+              notification={notification}
+              colored={colored}
+              to={toNotification ? toNotification(notification) : undefined}
+              onClick={onNotificationClick ? onNotificationClick(notification) : undefined}
+              href={notificationHref ? notificationHref(notification) : undefined}
+              target={notificationTarget ? notificationTarget(notification) : undefined}
+            />
           </div>
         ))
       ) : (
